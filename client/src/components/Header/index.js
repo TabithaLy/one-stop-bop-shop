e61e2fa096a75bccfaee034658390be94b1e7035
Tabitha <spencersalmon.tabitha@gmail.com>
2022-09-29 13:15:20 -0400

    header/nav

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Auth from '../../utils/auth';
+import './style.css';
 
 const Header = () => {
   // const logout = (event) => {
@@ -17,7 +18,7 @@ const Header = () => {
   };
   if (Auth.loggedIn()) {
     return (
-      <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+      <header className="bg-primary text-white mb-4 py-3 flex-row align-center">
         <div className="container flex-row justify-space-between-lg justify-center align-center">
           <div>
             <Link className="text-light" to="/">
@@ -25,11 +26,12 @@ const Header = () => {
             </Link>
             <p className="m-0">Get into the mind of a programmer.</p>
           </div>
-          <div>
+          <div className="tabColor">
             <Box sx={{ width: '100%' }}>
-              <Tabs value={value} onChange={handleChange} aria-label="nav tabs example">
-                <Tab label="Signup" href="/products" />
-                <Tab label="Login" href="/logout" />
+              <Tabs value={value} onChange={handleChange} aria-label="nav tabs" textColor="inherit" width="200" height="200">
+                <Tab><img src="OneStopBopShop.jpeg" alt="logo"></img></Tab>
+                <Tab className="tab" label="Signup" href="/products" />
+                <Tab className="tab" label="Login" href="/logout" />
               </Tabs>
             </Box>
           </div>
@@ -38,17 +40,18 @@ const Header = () => {
     );
   } else {
     return (
-      <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+      <header className="bg-primary text-white mb-4 py-3 flex-row align-center">
         <div className="container flex-row justify-space-between-lg justify-center align-center">
           <div>
             <h1 className="m-0">One Stop Bop Shop</h1>
             <p className="m-0">Let the record play!</p>
           </div>
-          <div>
+          <div className="tabColor">
             <Box sx={{ width: '100%' }}>
-              <Tabs value={value} onChange={handleChange} aria-label="nav tabs example">
-                <Tab label="Signup" href="/signup" />
-                <Tab label="Login" href="/login" />
+              <Tabs value={value} onChange={handleChange} aria-label="nav tabs" textColor="inherit">
+                <Tab><img src="OneStopBopShop.jpeg" alt="logo"></img></Tab>
+                <Tab className="tab" label="Signup" href="/signup" />
+                <Tab className="tab" label="Login" href="/login" />
               </Tabs>
             </Box>
           </div>
