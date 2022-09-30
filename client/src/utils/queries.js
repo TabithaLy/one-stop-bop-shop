d2e53f46f84404bd72de7814a3d99dfaf63ea81b
Tabitha <spencersalmon.tabitha@gmail.com>
2022-09-30 10:06:46 -0400

    material ui success

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,21 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
+
 export const QUERY_CHECKOUT = gql`
   query getCheckout($vinyls: [ID]!) {
     checkout(vinyls: $vinyls) {
