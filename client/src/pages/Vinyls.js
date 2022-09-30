08a63139bea899da25312b78e3ff4e6bd52f2e7d
emilyerose <emilyrose0021@gmail.com>
2022-09-30 13:36:23 -0400

    save changes

--- client/src/pages/Vinyls.js
+++ client/src/pages/Vinyls.js
@@ -1,13 +1,14 @@
 import React from "react";
 import VinylList from "../components/VinylList";
 import Header from "../components/Header"
+import Cart from "../components/Cart"
 //import Cart from "../components/Cart";
 
 const Vinyls = () => {
   return (
     <div className="container">
-      <Header/>
       <VinylList />
+      <Cart />
     </div>
   );
 };
