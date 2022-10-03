fa51a1c1aad163dbfc6726b0fbf3f9ea77e685ac
natesoum <soumnathun@gmail.com>
2022-10-02 23:33:43 -0400

    data shows up on single product page

--- client/src/components/SingleProduct/index.js
+++ client/src/components/SingleProduct/index.js
@@ -1,19 +1,30 @@
+import "./index.css";
 import React, { useEffect } from "react";
-import { QUERY_VINYLS } from "../../utils/queries";
+import { QUERY_BY_ID } from "../../utils/queries";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 
 function SingleProduct() {
-    const { _id } = useParams();
-    const { loading, error, data } = useQuery(QUERY_VINYLS);
-    if (loading) return <p>loading</p>;
+  const { _id } = useParams();
+  const { loading, error, data } = useQuery(QUERY_BY_ID, {
+    variables: { _id },
+  });
 
-    return (
-        <>
-        <h1>Album ID: { _id }</h1>
-  
-        </>
-    );
+  useEffect(() => {
+    if (data) {
+      console.log("=====data====", data);
+    }
+  }, [data]);
+
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="image-container">
+          <img className="image" alt="album cover" src={`https://res.cloudinary.com/daheygjio/image/upload/v1664415401/albumcovers/${data.vinyl.image}`} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SingleProduct;
